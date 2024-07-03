8cf697ee96349cbe811bdab0f7ff69165c334d38
Nikita kumari <itsnikita2303@gmail.com>
2024-07-03 08:51:00 +0530

    fullstack

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+}));
 app.use(express.json());
 
 dotenv.config();
@@ -30,6 +35,14 @@ try {
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+//deployment
+if(process.env.NODE_ENV === "production"){
+    const dirPath = path.resolve();
+    app.use(express.static("Frontend/dist"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath,"Frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
