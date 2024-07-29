19cf1284623a8026bb92802ed04d7cd6370d589a
Nikita <101445944+itsnikita2303@users.noreply.github.com>
2024-07-29 15:55:58 +0530

    Update Course.jsx

--- Backend/Frontend/src/components/Course.jsx
+++ Backend/Frontend/src/components/Course.jsx
@@ -25,13 +25,9 @@ function Course() {
             <span className="text-pink-500"> Here! :)</span>
           </h1>
           <p className="mt-12">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-            assumenda? Repellendus, iste corrupti? Tempore laudantium
-            repellendus accusamus accusantium sed architecto odio, nisi expedita
-            quas quidem nesciunt debitis dolore non aspernatur praesentium
-            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-            consequatur!
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequently, assumed? Repellent, that corrupt?
+            Time laudatory repellent accused accusantium but architect hates, unless expedited somewhat indeed does not know the debts of pain is
+            not spiny present assumed to be, perspiring, explained follow affectionate soul. To us, someone rejected are they who great follow!
           </p>
           <Link to="/">
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
